4cae56923cddf60fda781cb4dc2a754b38149f3e
c44rson <ccabre11@lion.lmu.edu>
2023-12-30 14:54:43 -0800

    email works ok formatting whatever

--- src/App.js
+++ src/App.js
@@ -4,41 +4,53 @@ function App() {
   return (
     <div className="global">
       <div className="header">
-        <h1>Carson Cabrera's Portfolio Website</h1>
-        <h2>I currently work for [insert your company name here]</h2>
+        <div className="title">
+          <h1>Carson Cabrera</h1>
+        </div>
+        <div className="emailForm">
+          <h3>Email Me:</h3>
+          <form
+            action="https://formsubmit.co/0d8407458399b4d6df4b7ce1585e6905"
+            method="POST"
+          >
+            <section>
+              <input type="text" name="Name" placeholder="Full Name" required />
+              <input type="email" name="Email" placeholder="Email" required />
+            </section>
+            <section>
+              <textarea
+                name="Message"
+                placeholder="Message"
+                required
+              ></textarea>
+            </section>
+            <button target="_blank" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
       <div className="body">
         <div className="interactive">
-          <form
-            action="mailto:ccabre11@lion.lmu.edu"
-            method="post"
-            encType="text/plain"
-          >
-            Student Name:
-            <input type="text" name="name" />
-            Student Subject:
-            <input type="text" name="subject" />
-            <input type="submit" value="Send" />
-          </form>
+          <h2>Interactive Thing Goes Here</h2>
+          <p>TODO INTERACTIVE GAME</p>
         </div>
         <div className="projects">
-          <p>
-            Projects Projects Projects Projects Projects Projects Projects{" "}
-            Projects Projects Projects Projects Projects Projects Projects{" "}
-            Projects Projects Projects Projects Projects Projects Projects{" "}
-            Projects Projects Projects Projects Projects Projects Projects{" "}
-            Projects Projects Projects Projects Projects Projects Projects{" "}
-            Projects Projects Projects Projects Projects Projects Projects{" "}
-          </p>
+          <h2>My Projects</h2>
+          <a href="https://github.com/c44rson" target="_blank">
+            Github Repo
+          </a>
+          <p>TODO ADD PROJECT PREVIEWS</p>
         </div>
         <div className="aboutme">
+          <h2>About Me</h2>
           <p>
-            About Me Bozo Foobar About Me Bozo Foobar About Me Bozo Foobar About
-            Me Bozo Foobar About Me Bozo Foobar About Me Bozo Foobar About Me
-            Bozo Foobar About Me Bozo Foobar About Me Bozo Foobar About Me Bozo
-            Foobar About Me Bozo Foobar About Me Bozo Foobar About Me Bozo
-            Foobar About Me Bozo Foobar About Me Bozo Foobar About Me Bozo
-            Foobar About Me Bozo Foobar About Me Bozo Foobar{" "}
+            I am a 19 year old B.S. Computer Science student at Loyola Marymount
+            University. I specialize in Full Stack Web Development and writing
+            programs in Python. Besides my programming endeavors I am on the
+            Loyola Rugby team, enjoy amateur Powerlifting, and casually run. In
+            the near future, I want to work a job at a tech company as a
+            software engineer. Long term, I want to take over the world.
           </p>
         </div>
       </div>
