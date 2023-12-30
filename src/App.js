9c0baa036774150d9e9366735e4bdf191c7c2f42
c44rson <ccabre11@lion.lmu.edu>
2023-12-30 11:37:41 -0800

    basic temp done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import "./App.css";
 
 function App() {
-  return <p>No Job Experience ROFL</p>;
+  return (
+    <div className="global">
+      <div className="header">
+        <h1>Carson Cabrera's Portfolio Website</h1>
+        <h2>I currently work for [insert your company name here]</h2>
+      </div>
+      <div className="body">
+        <div className="interactive">
+          <form
+            action="mailto:ccabre11@lion.lmu.edu"
+            method="post"
+            encType="text/plain"
+          >
+            Student Name:
+            <input type="text" name="name" />
+            Student Subject:
+            <input type="text" name="subject" />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+        <div className="projects">
+          <p>
+            Projects Projects Projects Projects Projects Projects Projects{" "}
+            Projects Projects Projects Projects Projects Projects Projects{" "}
+            Projects Projects Projects Projects Projects Projects Projects{" "}
+            Projects Projects Projects Projects Projects Projects Projects{" "}
+            Projects Projects Projects Projects Projects Projects Projects{" "}
+            Projects Projects Projects Projects Projects Projects Projects{" "}
+          </p>
+        </div>
+        <div className="aboutme">
+          <p>
+            About Me Bozo Foobar About Me Bozo Foobar About Me Bozo Foobar About
+            Me Bozo Foobar About Me Bozo Foobar About Me Bozo Foobar About Me
+            Bozo Foobar About Me Bozo Foobar About Me Bozo Foobar About Me Bozo
+            Foobar About Me Bozo Foobar About Me Bozo Foobar About Me Bozo
+            Foobar About Me Bozo Foobar About Me Bozo Foobar About Me Bozo
+            Foobar About Me Bozo Foobar About Me Bozo Foobar{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
