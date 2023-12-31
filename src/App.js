00e80e6816bca632dc93f1cb2785608b45aea6f7
c44rson <ccabre11@lion.lmu.edu>
2023-12-31 01:22:21 -0800

    abstraction of contact done
    now just record vids and the website is ready for showtime!

--- src/App.js
+++ src/App.js
@@ -1,88 +1,89 @@
 import "./App.css";
 import lmu from "./images/lmu.png";
 import me from "./images/c4rson.png";
+import leet from "./images/leetcode.png";
+import git from "./images/github.png";
+import { useState } from "react";
+import Contact from "./Contact";
 
 function App() {
+  const [home, setHome] = useState(true);
+  const [contact, setContact] = useState(false);
   return (
-    <div className="global">
+    <div className="god">
       <div className="header">
-        <img className="lmu" src={lmu} alt="LMU" />
-        <h1>Carson Cabrera</h1>
-      </div>
-      <div className="projects">
-        <h2>My Projects</h2>
-        <a href="https://github.com/c44rson" target="_blank" rel="noreferrer">
-          Github Repo
-        </a>
-        <p>
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-          PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS PROJECT DEMOS
-        </p>
-      </div>
-      <div className="aboutme">
-        <img className="me" src={me} alt="Carson" />
-        <p>
-          Carson Cabrera is a 19-year-old B.S. Computer Science major, Business
-          Administration minor student at Loyola Marymount University. He is
-          from the San Fernando Valley in Los Angeles, CA. His specialties are
-          Full-Stack Web Development and writing fun programs in Python. Besides
-          his programming endeavors, he is on the LMU Rugby team, enjoys amateur
-          Powerlifting, is a semi-pro Counter-Strike player, and a casual
-          runner. In the near future, he wants to get hired by any tech company,
-          in any field. Long term... world domination?
-        </p>
-      </div>
-      <div className="connect">
-        <h4>Links</h4>
-        <p>LINKS LINKS LINKS LINKS LINKS</p>
-        <p>LINKS LINKS LINKS LINKS LINKS</p>
-        <p>LINKS LINKS LINKS LINKS LINKS</p>
-        <p>LINKS LINKS LINKS LINKS LINKS</p>
-      </div>
-      <div className="emailForm">
-        <h4>Email Me:</h4>
-        <form
-          action="https://formsubmit.co/0d8407458399b4d6df4b7ce1585e6905"
-          method="POST"
+        <img className="headerPhoto" src={lmu} alt="LMU" />
+        <h1
+          onClick={() => {
+            setHome(true);
+            setContact(false);
+          }}
         >
-          <div className="formInput">
-            <input type="text" name="Name" placeholder="Name" required />
-          </div>
-          <div className="formInput">
-            <input type="email" name="Email" placeholder="Email" required />
-          </div>
-          <div className="formInput">
-            <textarea
-              name="Message"
-              placeholder="What's Up?"
-              required
-            ></textarea>
-          </div>
-          <div className="formInput">
-            <button target="_blank" type="submit">
-              Send
-            </button>
-          </div>
-        </form>
+          Carson Cabrera
+        </h1>
+        <a
+          href="https://leetcode.com/c4444rson/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="headerPhoto"
+            src={leet}
+            alt="leetcode logo clickable"
+          />
+        </a>
+        <a href="https://github.com/c44rson" target="_blank" rel="noreferrer">
+          <img className="headerPhoto" src={git} alt="github logo clickable" />
+        </a>
+        <button
+          onClick={() => {
+            setHome(false);
+            setContact(true);
+          }}
+        >
+          Contact Me
+        </button>
       </div>
+      {!home && contact ? (
+        <Contact />
+      ) : (
+        <div className="content">
+          <div className="projects">
+            <h2>Project Preview</h2>
+            <div className="videos">
+              <iframe
+                width="750"
+                height="450"
+                title="project1"
+                src="https://www.youtube.com/embed/bfxA1dDNPgE"
+              ></iframe>
+              <h3>Title</h3>
+              <p>Description</p>
+              <iframe
+                width="750"
+                height="450"
+                title="project2"
+                src="https://www.youtube.com/embed/LJEOy9VQjkk"
+              ></iframe>
+              <h3>Title</h3>
+              <p>Description</p>
+            </div>
+          </div>
+          <div className="aboutme">
+            <img className="me" src={me} alt="Carson" />
+            <p>
+              Carson Cabrera is a 19-year-old B.S. Computer Science major,
+              Business Administration minor 2nd-year at Loyola Marymount
+              University. Born and raised in the San Fernando Valley, his
+              specialties are Full-Stack Web Development and writing fun
+              programs in Python. Besides his programming endeavors, he starts
+              on the LMU Rugby team, enjoys Powerlifting and runs long
+              distances. In the near future, he wants to get hired by any tech
+              company, in any field. Long term? World Domination.
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function Contact() {
+  return (
+    <div className="contactPage">
+      <h2>Contact Information</h2>
+      <h3>Email Me:</h3>
+      <form
+        action="https://formsubmit.co/0d8407458399b4d6df4b7ce1585e6905"
+        method="POST"
+      >
+        <div className="formInput">
+          <input type="text" name="Name" placeholder="Name" required />
+        </div>
+        <div className="formInput">
+          <input type="email" name="Email" placeholder="Email" required />
+        </div>
+        <div className="formInput">
+          <textarea
+            name="Message"
+            placeholder="What's Up?"
+            required
+            maxLength={250}
+          ></textarea>
+        </div>
+        <div className="formInput">
+          <button target="_blank" type="submit">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
